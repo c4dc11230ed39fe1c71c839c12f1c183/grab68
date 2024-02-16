5dce1d89899eabb5c6a1e080d8dff447028f4e0e
c4dc11230ed39fe1c71c839c12f1c183 <safestbox@gmail.com>
2024-02-16 06:26:46 +0100

    commit

--- routes/web.php
+++ routes/web.php
@@ -38,4 +38,5 @@ Route::controller(Grab68Controller::class)
         Route::get('endpoint-test-guzzle', 'testEndpointWithGuzzle')->name('endpoint-test-guzzle');
         Route::get('ty-gia-68-market/{type}', 'getTyGia68MarketPrice')->where('type', 'market|emoney|usdt')->name('ty-gia-68-price');
         Route::get('ty-gia-68-gold', 'getTyGia68GoldPrice')->name('ty-gia-68-gold');
+        Route::get('ty-gia-68-nice', 'getTyGia68NicePrice')->name('ty-gia-68-nice');
     });
