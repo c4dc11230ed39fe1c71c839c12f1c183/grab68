823b5f7066c56aa7c1a1144afe00c5b3b61ee9e5
c4dc11230ed39fe1c71c839c12f1c183 <safestbox@gmail.com>
2024-02-16 04:36:36 +0100

    commit

--- app/Http/Controllers/Grab68Controller.php
+++ app/Http/Controllers/Grab68Controller.php
@@ -32,11 +32,11 @@ class Grab68Controller extends Controller
     {
         $response = app('grab68')->scrapeJson($this->tyGia68API[$apiVersion]['market_price']);
 
-        if (!empty($respose['data']['data'])) {
+        if (!empty($response['data']['data'])) {
             dd('wtf');
             dump($response['data']['data']);
 
-            foreach ($respose['data']['data'] as $key => $value) {
+            foreach ($response['data']['data'] as $key => $value) {
                 $base = strtolower($value['from']);
                 $quote = strtolower($value['to']);
                 $name = $base . '/' . $quote;
