13073759b2748a436b5d9e896f8b65691f290014
c4dc11230ed39fe1c71c839c12f1c183 <safestbox@gmail.com>
2024-02-16 04:10:22 +0100

    commit

--- app/Http/Controllers/Grab68Controller.php
+++ app/Http/Controllers/Grab68Controller.php
@@ -30,7 +30,7 @@ class Grab68Controller extends Controller
 
     public function getTyGia68MarketPrice($apiVersion = 'v1')
     {
-        dd('test');
+        dd('testx');
         $response = app('grab68')->scrapeJson($this->tyGia68API[$apiVersion]['market_price']);
         if (!empty($respose['data']['data'])) {
             foreach ($respose['data']['data'] as $key => $value) {
